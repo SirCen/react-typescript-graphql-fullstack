83a63e5760df383796f449cc5cd875d5b5989551
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2021-04-12 22:28:52 +0100

    Created new User table

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const constants_1 = require("./constants");
 const Post_1 = require("./entities/Post");
+const User_1 = require("./entities/User");
 const path_1 = __importDefault(require("path"));
 exports.default = {
     migrations: {
@@ -13,7 +14,7 @@ exports.default = {
     },
     user: 'postgres',
     password: 'postgres',
-    entities: [Post_1.Post],
+    entities: [Post_1.Post, User_1.User],
     dbName: "lireddit",
     type: "postgresql",
     debug: !constants_1.__prod__,
