787bb238b1f3c3f7b226551bd751205daa0d8244
Chris Wilsher <52220166+SirCen@users.noreply.github.com>
2021-05-08 22:49:18 +0100

    Added cookie sessions

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,12 @@ import {buildSchema} from 'type-graphql';
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { MyContext } from "./types";
+
+
 
 const main = async () => {
     
@@ -15,14 +21,33 @@ const main = async () => {
     
     const app = express();
     
+    const RedisStore = connectRedis(session);
+    const redisClient = redis.createClient();
+    //add session middleware 
+    app.use(
+        session({
+            name: 'qid', //cookie name
+            store: new RedisStore({client: redisClient, disableTouch: true,}), //touch resets TTL
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 28,//28 days
+                httpOnly: true,
+                sameSite: 'lax', //csrf
+                secure: __prod__, //cookie only works in https
+            },
+            saveUninitialized: false,
+            secret: "cwjkhgzmfltmrkgmlrla", //change to environment var
+            resave: false,
+        })
+    );
+
     const apolloserver = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
-        context: () => ({em: orm.em})
+        context: ({req, res}): MyContext => ({em: orm.em, req, res}),
     });
-
+    //add apollo middleware
     apolloserver.applyMiddleware({app});
 
     app.listen(4000, () => {
